d8b5c149cea42797a94ef7728b258314df6b0ab6
Justin Turner <jmturner638@gmail.com>
2021-07-28 19:50:30 -0500

    second Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,11 +1,11 @@
-import numpy as np
+from math import sqrt
 
 def main():
     #write your code below this line
     num1 = int(input())
     num2 = int(input())
 
-    square_root = np.sqrt(num1 + num2)
+    square_root = sqrt(num1 + num2)
     print(square_root)
 
 if __name__ == '__main__':
